0dd3acca2eb88448ab57c8d546f2a66e30b34293
Your Name <your.email@example.com>
2025-08-19 14:40:11 +0500

    Improve code

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-// 
+//
 let inputE = document.querySelector(".num");
 let farenheitE = document.querySelector(".farenheit");
 let celsiusE = document.querySelector(".celsius");
@@ -7,25 +7,20 @@ let resultE = document.querySelector("p");
 let temp;
 
 const convert = () => {
-    if (farenheitE.checked) 
-        {
-        temp = parseFloat(inputE.value);
-        temp = temp * 9 / 5 + 32;
-        resultE.classList.remove("hide");
-        resultE.innerText = "Result: "+temp.toFixed(1) + "°F";
-    }
-     else if (celsiusE.checked)
-         {
-        temp = parseFloat(inputE.value);
-        temp = (temp - 32) * (5 / 9);
-        resultE.classList.remove("hide");
-        resultE.innerText = "Result: "+temp.toFixed(1) + "°C";
-    } 
-    else 
-    {
-        resultE.classList.remove("hide");
-        resultE.innerText = "Please select a unit";
-    }
+  if (farenheitE.checked) {
+    temp = parseFloat(inputE.value);
+    temp = (temp * 9) / 5 + 32;
+    resultE.classList.remove("hide");
+    resultE.innerText = "Result: " + temp.toFixed(1) + "°F";
+  } else if (celsiusE.checked) {
+    temp = parseFloat(inputE.value);
+    temp = (temp - 32) * (5 / 9);
+    resultE.classList.remove("hide");
+    resultE.innerText = "Result: " + temp.toFixed(1) + "°C";
+  } else {
+    resultE.classList.remove("hide");
+    resultE.innerText = "Please select a unit";
+  }
 };
 
 buttonE.addEventListener("click", convert);
